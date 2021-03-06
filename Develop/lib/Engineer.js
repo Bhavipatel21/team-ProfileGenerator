d7b4550a0a059c513b2ca620babd70513c21bf3b
Bhavika Patel <bhavij28@gmail.com>
2021-03-06 17:43:47 -0500

    initial commit

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,20 @@
+const Employee = require('./Employee');
+
+
+class Engineer extends Employee {
+
+constructor (id,name,email,github){
+
+super(id,name,email);
+
+this.github=github;
+
+
+}
+
+getGithub()
+
+getRole()
+
+}
+
